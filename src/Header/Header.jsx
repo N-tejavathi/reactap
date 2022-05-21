022d6abdefdd8bed1eb5f65a853ce7e6adb90ef1
thejavathi <tejavati@gmail.com>
2022-05-21 22:17:24 +0530

    react app added

---
+++ src/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="header bg-primary text-center text-whit">
+      My First React Application
+    </div>
+  );
+};
+
+export default template;
